390b38b5aee290322c95e583cd89b3e94be3807f
EC2 Default User <amtiotuico@gmail.com>
2022-02-08 01:06:16 +0000

    modified invalid input function to take care of edge cases

--- lesson3/problem5.js
+++ lesson3/problem5.js
@@ -41,10 +41,8 @@ Problem:
 (encoding function)
 given two inputs, a message string and integer representing how many rows/rails are needed,
 return a string that encodes the message using n given rows
-
 (decoding function)
 given an encoded message string, return the decoded message as a string
-
 rules/requirements:
 create a function that encodes a message given the message and how many rows/rails
 create another function that decodes a given message
@@ -54,10 +52,8 @@ until we hit the bottom/last row and start moving upward (one space to the right
 and one up until we hit the first row) and continue moving down and up until
 the end of the string
 we ignore/skip spaces
-
 input: string and int (encoding) string (decoding)
 output: string (encoding and decoding)
-
 questions:
 Will the input always be letters and spaces only?
 How do we handle non string inputs?
@@ -79,49 +75,38 @@ Does the output need to be a rectangle?
   - the same amount of chars on each line?
 Should we output each line on its own or in one long string?
 What if the second input is a string but contains a number?
-
 Examples / test cases:
 "WE ARE DISCOVERED FLEE AT ONCE"
-
 W . . . E . . . C . . . R . . . L . . . T . . . E
 . E . R . D . S . O . E . E . F . E . A . O . C .
 . . A . . . I . . . V . . . D . . . E . . . N . .
-
 WECRLTEERDSOEEFEAOCAIVDEN
-
 length of string = 30
 length of just letters (w/o spaces) = 25
 length of fence = 25 chars
 length of space between each letter on 1st row = 3 in between, 0 from start and end
 length of space between each letter on 2nd row = 1 from the start and end and in between
 length of space between each letter on 3rd row = 2 from the start and end, 3 in between
-
 1st row has space for 7 letters
 2nd row has space for 12 letters
 3rd row has space for 6 letters
-
-
 console.log(railCipherEncode('WE ARE DISCOVERED FLEE AT ONCE', 2))
 W . A . E . I . C . V . R . D . L . E . T . N . E
 . E . R . D . S . O . E . E . F . E . A . O . C .
-
 console.log(railCipherEncode('WE ARE DISCOVERED FLEE AT ONCE', 3))
 W . . . E . . . C . . . R . . . L . . . T . . . E
 . E . R . D . S . O . E . E . F . E . A . O . C .
 . . A . . . I . . . V . . . D . . . E . . . N . .
-
 console.log(railCipherEncode('WE ARE DISCOVERED FLEE AT ONCE', 4))
 W	.	.	.	.	.	I	.	.	.	.	.	R	.	.	.	.	.	E	.	.	.	.	.	E
 .	E	.	.	.	D	.	S	.	.	.	E	.	E	.	.	.	E	.	A	.	.	.	C	.
 .	.	A	.	E	.	.	.	C	.	V	.	.	.	D	.	L	.	.	.	T	.	N	.	.
 .	.	.	R	.	.	.	.	.	O	.	.	.	.	.	F	.	.	.	.	.	O	.	.	.
-
 letter indexes per row:
 1st row . 0 6 12 18 24
 2nd row . 1 5 7 11 13 17 19 23
 3rd row . 2 4 8 10 14 16 20 22
 4th row . 3 9 15 21
-
 console.log(railCipherEncode('WE ARE DISCOVERED FLEE AT ONCE', 5)
 W	.	.	.	.	.	.	.	C	.	.	.	.	.	.	.	L	.	.	.	.	.	.	.	E
 .	E	.	.	.	.	.	S	.	O	.	.	.	.	.	F	.	E	.	.	.	.	.	C	.
@@ -134,7 +119,6 @@ letter indexes per row:
 3rd row . 2 6 10 14 18 22
 4th row . 3 5 11 13 19 21
 5th row . 4 12 20
-
 console.log(railCipherEncode('WE ARE DISCOVERED FLEE AT ONCE', 6)
 W	.	.	.	.	.	.	.	.	.	V	.	.	.	.	.	.	.	.	.	T	.	.	.	.
 .	E	.	.	.	.	.	.	.	O	.	E	.	.	.	.	.	.	.	A	.	O	.	.	.
@@ -142,78 +126,60 @@ W	.	.	.	.	.	.	.	.	.	V	.	.	.	.	.	.	.	.	.	T	.	.	.	.
 .	.	.	R	.	.	.	S	.	.	.	.	.	E	.	.	.	E	.	.	.	.	.	C	.
 .	.	.	.	E	.	I	.	.	.	.	.	.	.	D	.	L	.	.	.	.	.	.	.	E
 .	.	.	.	.	D	.	.	.	.	.	.	.	.	.	F	.	.	.	.	.	.	.	.	.
-
 notes:
 first row. start on index 0 then
 last row . the character is placed on the nth index . 1 (n being the # rows input)
-
-
 console.log(railCipherEncode('hello', 2))
 h . l . o
 . e . l .
-
 console.log(railCipherEncode('hello', 3))
 h . . . o
 . e . l .
 . . l . .
-
 console.log(railCipherEncode('hello', 4))
 h . . . .
 . e . . .
 . . l . o
 . . . l .
-
 console.log(railCipherEncode('hello', 5))
 h . . . .
 . e . . .
 . . l . .
 . . . l .
 . . . . o
-
 console.log(railCipherEncode('moon', 2))
 m . o .
 . o . n
-
-
 console.log(railCipherEncode('moon', 3))
 m . . .
 . o . n
 . . o .
-
-
 console.log(railCipherEncode('moon', 4))
 m . . .
 . o . .
 . . o .
 . . . n
-
 console.log(railCipherEncode('moon', 5))
 m . . .
 . o . .
 . . o .
 . . . n
 . . . .
-
 Data Structures:
 string(input/output)
 number/integer (input)
 arrays
 regex?
-
 Algorithm:
 - guard clause(s)
   - if msg str in not string type and int is not number type, return 'invalid input'
   - if msg str input includes anything other than letters and spaces, return 'invalid input'
   - if rails num input is less than 2 return 'invalid input'
-
 - clean msg str
 - remove anything that isn't a letter a-zA-Z (case insensitive)
-
 - if cleaned str is < 2, return 'invalid input'
-
 - split cleaned str into array of chars `letters`
 - initialize result array of n sub arrays Array(n).fill([])
-
 - create rows
 - initialize `row` to 0
 - initialize `directionUp` to `false
@@ -226,12 +192,11 @@ Algorithm:
 - log each row
 - iterate through `result`
   - for each subarry, join it and log it
-
-
 */
 
 function invalidEncodeInput(msg, rows) {
-  return typeof msg !== 'string' || /[^a-z ]+/gi.exec(msg) || rows < 2;
+  return typeof msg !== 'string' || /[^a-z ]+/gi.exec(msg) || rows < 2
+          || (typeof rows !== 'number' || Number.isNaN(rows) || rows === Infinity || rows === -Infinity);
 }
 
 function cleanStr(msg) {
@@ -358,27 +323,22 @@ algorithm:
 - guard clause(s)
   - return 'invalid input' if msg isn't a string or rows is less than 2 or msg includes
     anything but letters
-
 - initialize resultRows = []
 - push in an empty array given `rows` amount of times
   -for ex if given 3 then [[], [], []]
 - initialize letters to an array of each char of msg msg.split('')
-
 - push letters into first row x times
   - Round(length of msg input / given rows ) - 1 times
   - use shift to get rid of the item but the return value of shift to push into first row
-
 - push letters into last row x times
   - Round(length of msg input / given rows ) - 2 times
   - use pop to get rid of the item but the return value of pop to push into last row
-
 - get remaining length of `letters`, letters.length
 - get remaining rows, rows - 2
 - get letters per row length divided by remaining rows
 - use a for loop to iterate starting at row 1 until we reach `rows` - 1
   - reassign the current resultRow[row] to the letters starting from 0 to letters per row
     - use splice to mutate letters so we can get rid of the used letters
-
 - initialize row to 0
 - initialize directionUp to false
 - iterate through letters using map for transformation
@@ -389,7 +349,6 @@ algorithm:
       - direction up should be false
     - if row is equal to rows - 1
       - directionup should be true
-
 */
 
 function invalidDecodeInput(msg, rows) {
